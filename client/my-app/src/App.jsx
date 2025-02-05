b1f725067822c462336b31edd60c9371c21224a4
sharmaJi <prakashdeep2902@gmail.com>
2025-02-05 07:53:41 +0530

    remove all unwanted file from client

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,5 +1,10 @@
-const App = () => {
-  return <h1>Hello, React!</h1>;
-};
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function App() {
+  return <h1>Hello, World!</h1>;
+}
 export default App;
+
+
+
