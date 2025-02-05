f0a3c82ca2246d865ba9700db7c7165be420e869
sharmaJi <prakashdeep2902@gmail.com>
2025-02-05 07:10:13 +0530

    added commit on 2-5-25

--- server/app.js
+++ server/app.js
@@ -1,12 +1,49 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-const app = express();
+async function getData() {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return []; // Return an empty array in case of error
+  }
+}
 
-const port = 9090;
+async function startServer() {
+  const port = 9090;
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+      type Todo{
+      id:ID!,
+      title:String!
+      completed:Boolean
+    }
+     type Query {
+       getTodos:[Todo]
+      }
+    `,
+    resolvers: {
+      Query: {
+        getTodos: async () => await getData(),
+      },
+    },
+  });
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cors());
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
 
-app.get("/", (req, res) => {
-  res.send("<h1>GraphQl<h1>");
-});
-app.listen(port, () => {
-  console.log(`app is running port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`app is running on port ${port}`);
+  });
+}
+
+startServer();
